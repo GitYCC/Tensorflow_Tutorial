cc13368a4d0ddfd700b8157a66224a5e80e9e954
Yi-Chang Chen <ycchen@YCChens-MBP.local>
2017-11-25 22:27:51 +0800

    update 171125_01

--- code/05_1_word2vec_SkipGram.py
+++ code/05_1_word2vec_SkipGram.py
@@ -15,42 +15,42 @@ import time
 VOCABULARY_SIZE = 100000
 
 def maybe_download(url,filename, expected_bytes):
-  """Download a file if not present, and make sure it's the right size."""
-  if not os.path.exists(filename):
-    filename, _ = urlretrieve(url, filename)
-  statinfo = os.stat(filename)
-  if statinfo.st_size == expected_bytes:
-    print('Found and verified %s' % filename)
-  else:
-    print(statinfo.st_size)
-    raise Exception(
-      'Failed to verify ' + filename + '. Can you get to it with a browser?')
-  return filename
+    """Download a file if not present, and make sure it's the right size."""
+    if not os.path.exists(filename):
+        filename, _ = urlretrieve(url, filename)
+    statinfo = os.stat(filename)
+    if statinfo.st_size == expected_bytes:
+        print('Found and verified %s' % filename)
+    else:
+        print(statinfo.st_size)
+        raise Exception(
+          'Failed to verify ' + filename + '. Can you get to it with a browser?')
+    return filename
 
 def read_data(filename):
-  """Extract the first file enclosed in a zip file as a list of words"""
-  with zipfile.ZipFile(filename) as f:
-    data = tf.compat.as_str(f.read(f.namelist()[0])).split()
-  return data
+    """Extract the first file enclosed in a zip file as a list of words"""
+    with zipfile.ZipFile(filename) as f:
+        data = tf.compat.as_str(f.read(f.namelist()[0])).split()
+    return data
   
 def build_dataset(words,vocabulary_size=VOCABULARY_SIZE):
-  count = [['UNK', -1]]
-  count.extend(collections.Counter(words).most_common(vocabulary_size - 1))
-  dictionary = dict()
-  for word, _ in count:
-    dictionary[word] = len(dictionary)
-  data = list()
-  unk_count = 0
-  for word in words:
-    if word in dictionary:
-      index = dictionary[word]
-    else:
-      index = 0  # dictionary['UNK']
-      unk_count = unk_count + 1
-    data.append(index)
-  count[0][1] = unk_count
-  reverse_dictionary = dict(zip(dictionary.values(), dictionary.keys())) 
-  return data, count, dictionary, reverse_dictionary
+    count = [['UNK', -1]]
+    count.extend(collections.Counter(words).most_common(vocabulary_size - 1))
+    dictionary = dict()
+    for word, _ in count:
+        dictionary[word] = len(dictionary)
+    data = list()
+    unk_count = 0
+    for word in words:
+        if word in dictionary:
+            index = dictionary[word]
+        else:
+            index = 0  # dictionary['UNK']
+            unk_count = unk_count + 1
+        data.append(index)
+    count[0][1] = unk_count
+    reverse_dictionary = dict(zip(dictionary.values(), dictionary.keys())) 
+    return data, count, dictionary, reverse_dictionary
 
 
 def skip_gram_batch_generator(data,batch_size,num_skips,skip_window):
